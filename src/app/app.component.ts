63ee73cbba06e1b709e69451b8de6b25a9c9354b
Elias Valenzuela <elias.ariel.valenzuela@gmail.com>
2025-04-22 16:26:15 -0300

    footer y navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SliderComponent } from "./slider/slider.component";
+import { TechStackComponent } from "./tech-stack/tech-stack.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, SliderComponent, TechStackComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
